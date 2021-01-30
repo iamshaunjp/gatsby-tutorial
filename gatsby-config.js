c149e5f8f60c5f3c6f11106352d2cccfcaa4ea24
Shaun <shaun@thenetninja.co.uk>
2021-01-30 15:15:06 +0000

    lesson-9

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Web Warrior',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright 2021 Gatsby Ninja'
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from "../components/layout"
 import styles from '../styles/home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
+  const { title, description } = data.site.siteMetadata
+
   return (
     <Layout>
       <section className={styles.header}>
@@ -14,7 +17,19 @@ export default function Home() {
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
+        <p>{ title } - { description }</p>
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
