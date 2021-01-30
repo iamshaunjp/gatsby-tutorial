90b7a4d2556abf60a180325a7539285f98f5b16d
Shaun <shaun@thenetninja.co.uk>
2021-01-30 17:09:46 +0000

    lesson-9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql, Link } from 'gatsby'
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import styles from '../styles/home.module.css'
 
 export default function Home({ data }) {
