4d2a4641d74ad1841c28900b4d479a77457d1f74
Shaun <shaun@thenetninja.co.uk>
2021-01-31 11:45:31 +0000

    lesson-11

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,22 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `notes`,
+        path: `${__dirname}/src/notes/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
   siteMetadata: {
     title: 'Web Warrior',
     description: 'web dev portfolio',
