8003ec0548bb5cfb76dfa4de27ae8e2d61206557
Shaun <shaun@thenetninja.co.uk>
2021-02-01 12:26:34 +0000

    lesson-17

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import styles from '../../styles/projects.module.css'
 import { Link, graphql } from 'gatsby'
+import Img from "gatsby-image"
 
 const Projects = ({ data }) => {
   console.log(data)
@@ -17,6 +18,7 @@ const Projects = ({ data }) => {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{ project.frontmatter.title }</h3>
                 <p>{ project.frontmatter.stack }</p>
               </div>
@@ -40,6 +42,13 @@ export const query = graphql`
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
