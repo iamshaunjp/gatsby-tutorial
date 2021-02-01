a014f6d9eae4077f91b60fd7280c2683a5d458b1
Shaun <shaun@thenetninja.co.uk>
2021-02-01 17:33:15 +0000

    lesson-18

---
+++ src/templates/project-details.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Img from "gatsby-image"
+import Layout from "../components/Layout"
+import styles from '../styles/project-details.module.css'
+
+const ProjectDetails = () => {
+  return (
+    <Layout>
+      <div className={styles.details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={styles.featured}>
+          <Img fluid={} />
+        </div>
+        <div className={styles.html} dangerouslySetInnerHTML={} />
+      </div>
+    </Layout>
+  )
+}
+ 
+export default ProjectDetails
