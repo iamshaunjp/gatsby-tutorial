2402656541bd04458c40329954eb1ea4ec7267b0
royeradames <royeraadames@gmail.com>
2021-08-15 16:22:30 -0400

    📦fix error path of D:\web-projects\classes\gatsby-tutorial/src/notes/ not existing

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,21 +11,14 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `notes`,
-        path: `${__dirname}/src/notes/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
       },
     },
   ],
   siteMetadata: {
-    title: 'Web Warrior',
-    description: 'web dev portfolio',
-    copyright: 'This website is copyright 2021 Web Warrior'
+    title: "Web Warrior",
+    description: "web dev portfolio",
+    copyright: "This website is copyright 2021 Web Warrior",
   },
 }
